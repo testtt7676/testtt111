b6166d8212b31134cc1ec1c7cb7afaab4ad4b7dc
testtt7676 <153448911+testtt7676@users.noreply.github.com>
2025-06-30 12:20:56 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+testsdfdsf@gmail.com
+dasdasdas@shopify.com
+
+https://shpoify.com
+
+http://netfity.net
+
+dasdasdas@shopify123.com
+testsdfdsf@gmailssss.com
+
+netfity123.net/authorize
+
+https://netfity12323.com/authorize
